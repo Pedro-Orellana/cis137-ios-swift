c07a60136da630412f7b11a69fb15d57abead79f
Pedro Orellana <pedrorellana21@gmail.com>
2026-08-19 14:59:15 -0700

    Initial commit

---
+++ Hello/Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Pedro Orellana on 8/19/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
